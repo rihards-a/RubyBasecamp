bbef3ce66c77a69f252a293faa25da374772b05d
Rihards <rihards.abolins1@gmail.com>
2024-10-28 19:16:25 +0200

    created a user sign up page with basic error validation

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,37 @@
+class UserController < ApplicationController
+    def show
+      # will have to make this to display user information later
+    end
+
+    def new
+        # instance variable with @, so the variable is available outside of this method
+        @user = User.new
+    end
+
+    def create
+        # not using params[:user] because if we want to include future flags like is_admin, we do not want to allow the user to control that, we use a helper function
+        @user = User.new(user_params)
+        if @user.save
+            # WILL HAVE TO CHANGE TO USER_PATH TO #SHOW CONTROLLER WHEN IT IS SET UP
+            redirect_to root_path, notice: "successfully created account"
+        else
+            render :new # goes back to the sign up page, error messages are handled by forms
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
+
+
+=begin
+INSIGHTS
+1.
+    # params references the post request parameters from the form from #new / alternatively it can receive URL input
+    render plain: params[:user]
+    # :user because we were working with @user and we only want to display its contents not this whole string that includes token and commit hashes:
+    # {"authenticity_token"=>"JPEasYJrii0SsH8iYvXN-Igg-uUxIZFWoR-9SHy3K8NxAdjW3pA-gP2mDZUsBRuurbxDkRwqU7-7majGH-GQFQ", "user"=>{"email"=>"123as@asd", "password"=>"", "password_confirmation"=>""}, "commit"=>"Create User", "controller"=>"user", "action"=>"create"}
+
+=end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htmlRails.application.routes.draw do
 Rails.application.routes.draw do
-  get "sign_up" => "user/new"
+  get "sign_up", to: "user#new"
+  post "sign_up", to: "user#create"
+  get "user", to: "user#show"
 
-  # GoRails tutorial
   # get "about", to: "about#index"
   get "about_renamed", to: "about#index", as: :about # the _path will return this route, even if the url name gets changed
 
   get "/", to: "main#index", as: :root
   # Same as root to: "" || root ""
-
-  #get ""
 end

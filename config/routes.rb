bebcf2a5bd3161ea6dfa74f98d3b50d1dc8b9dfb
Rihards <rihards.abolins1@gmail.com>
2024-11-06 11:59:46 +0200

    changed to layout of the routes for easier management

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htmlRails.application.routes.draw do
 Rails.application.routes.draw do
+  get "about_renamed", to: "about#index", as: :about # the _path will return this route, even if the url name gets changed == # get "about", to: "about#index"
+
   # User registration and session management
-  get "sign_up", to: "user#new"
+  get "sign_up", to: "user#new" # get for filling out forms and post for submitting them
   post "sign_up", to: "user#create"
 
-  get "sign_in", to: "sessions#new" # get for filling out forms and post for submitting them
+  get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
 
-
   get "user", to: "user#show" # to implement - display user profile details
-
-  get "about_renamed", to: "about#index", as: :about # the _path will return this route, even if the url name gets changed == # get "about", to: "about#index"
 
   get "/", to: "main#index", as: :root # Same as root to: "" || root ""
 end
